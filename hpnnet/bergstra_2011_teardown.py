75d3d868281f4c62eadc4a847b147664fc9322f2
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2014-10-08 12:33:01 +0200

    Update configuration files

--- hpnnet/bergstra_2011_teardown.py
+++ hpnnet/bergstra_2011_teardown.py
@@ -2,6 +2,6 @@ import sys
 import shutil
 import os
 
-optimizer_dir_in_experiment = sys.argv[1]
+optimizer_dir_in_experiment = os.getcwd()
 print "Going to remove", os.path.join(optimizer_dir_in_experiment, "compiledir")
 shutil.rmtree(os.path.join(optimizer_dir_in_experiment, "compiledir"))
